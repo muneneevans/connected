6760fb2e3d2a63e14b47a111feb58e8c1c37e4de
muneneevans <munene.evansk@gmail.com>
2018-08-31 14:08:59 +0300

    [ADD]: theme colors

--- src/Store/Configuration/theme.js
+++ src/Store/Configuration/theme.js
@@ -4,7 +4,7 @@ export const theme = {
 
   FONT_SIZE_SMALL: 12,
   FONT_SIZE_MEDIUM: 14,
-  FONT_SIZE_LARGE: 16,
+  FONT_SIZE_LARGE: 18,
   FONT_SIZE_MASSIVE: 24,
 
   FONT_WEIGHT_LIGHT: "200",
@@ -28,4 +28,43 @@ export const theme = {
   SECONDARY_TEXT_INPUT_BACKGROUND_COLOR: "#000000",
   PRIMARY_TEXT_INPUT_COLOR: "#000000",
   SECONDARY_TEXT_INPUT_COLOR: "#ffffff"
+};
+
+export const colorOptions = {
+  orange: {
+    faint: "#FFF3E0",
+    light: "#FFB74D",
+    medium: "#FF9800",
+    bold: "#EF6C00"
+  },
+  red: {
+    faint: "#FFEBEE",
+    light: "#E57373",
+    medium: "#F44336",
+    bold: "#C62828"
+  },
+  blue: {
+    faint: "#E3F2FD",
+    light: "#64B5F6",
+    medium: "#2196F3",
+    bold: "#1565C0"
+  },
+  cyan: {
+    faint: "#E0F7FA",
+    light: "#4DD0E1",
+    medium: "#00BCD4",
+    bold: "#00838F"
+  },
+  teal: {
+    faint: "#E0F2F1",
+    light: "#4DB6AC",
+    medium: "#009688",
+    bold: "#00695C"
+  },
+  gray: {
+    faint: "#FAFAFA",
+    light: "#E0E0E0",
+    medium: "#9E9E9E",
+    bold: "#424242"
+  }
 };
